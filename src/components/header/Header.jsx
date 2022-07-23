b4213c2f9025ed210011762f5ca5ce51d179c297
ericagong <48196721+ericagong@users.noreply.github.com>
2022-07-23 20:18:33 +0900

    Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const Header = (props) => {
+  return (
+    <header>
+      <div>My Todo List</div>
+      <div>React</div>
+    </header>
+  );
+};
+
+export default Header;
