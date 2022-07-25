f74bdb0e566c884305c59c74b9cd3795566bea91
ericagong <48196721+ericagong@users.noreply.github.com>
2022-07-25 19:21:34 +0900

    clean code

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -4,10 +4,6 @@ import { useDispatch } from "react-redux";
 import { updateTodo, removeTodo } from "../../redux/modules/todos";
 
 const Todo = ({ id, title, content, isDone }) => {
-  // isDone 여부에 따라 취소, 완료 조건부 렌더링
-  // TODO 왜 오류?
-  // const deleteBtn = React.useRef();
-  // const toggleBtn = React.useRef();
   const dispatch = useDispatch();
   const toggleHandler = () => {
     dispatch(updateTodo(id));
